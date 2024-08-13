0c17f3547c8e767cf74927c52fb26dd2711bd86b
kateNEO <glebova.caterina2012@yandex.ru>
2024-08-13 15:17:48 +0200

    .

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+let abs= Math.abs(n);
+    let str = String(abs);
+    let array=Array.from(str);
+    let newNumber=array.reverse().join('');
+    return Number(newNumber);
 }
+
